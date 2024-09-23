f5b1d9d2b679bd974f6ec6322b5e978ff85ba49a
SnowfallFox <icebornwhisker@gmail.com>
2024-09-23 13:35:43 +0000

    feat: added api functionality for /api/whoami requests

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req,res) => {
+  req.lang = req.headers['accept-language']
+  req.soft = req.headers['user-agent']
+  // req.ip = ip already, so doesn't need a var
+
+  res.json({"ipaddress":req.ip, "language":req.lang, "software":req.soft})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
